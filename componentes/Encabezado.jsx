6643cff4419113c4656d06f6a6b20585c382999b
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-02-04 22:36:19 -0600

    commit

--- componentes/Encabezado.jsx
+++ componentes/Encabezado.jsx
@@ -4,7 +4,7 @@ import { styles } from './Styles';
 export const Encabezado = () => {
   return (
     <View style={styles.encabezadoBienvenido}>
-      <Text style={styles.TextBienvenido}>Bienvenido</Text>
+      <Text style={styles.TextBienvenido}>Bienvenid@</Text>
     </View>
   );
 };
